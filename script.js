e1b52cea0d040cf917bc13506882a701d36b265b
Hugo Doloue <hugo.doloue@gmail.com>
2023-02-01 15:05:34 +0100

    Commit n°5

--- script.js
+++ script.js
@@ -15,6 +15,22 @@ return varTab;
 window.onload=function()
 {
 let TableauId= getAllId();
+display(TableauId);
 console.log(TableauId);
 }
 
+function display(arr)
+{
+for(i=0;i<28;i++) //balaye les 28 cases
+{
+var img= "url(images/" + i+".jpg)"; //fabrique l'image
+console.log(img);
+document.getElementById(arr[i]).style.backgroundImage=img;
+}
+
+
+
+
+
+
+}
